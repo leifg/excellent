5ce2e65d797a4bf0cdc2699cc54d332a4e72e306
Leif Gensert <leif@gensert.de>
2015-08-25 11:46:14 -0700

    use own spreadsheet for tests

--- spec/excellent_spec.exs
+++ spec/excellent_spec.exs
@@ -1,9 +1,27 @@
 defmodule ExcellentSpec do
   use ESpec
 
+  describe "#read" do
+    context "text only" do
+      let :expected_output do
+        [
+          ["1.A1", "1.B1", "1.C1", "1.D1"],
+          ["1.A2", "1.B2", "1.C2", "1.D2"],
+          ["1.A3", "1.B3", "1.C3", "1.D3"],
+          ["1.A4", "1.B4", "1.C4", "1.D4"],
+          ["1.A5", "1.B5", "1.C5", "1.D5"],
+        ]
+      end
+
+      it "returns contents as stream" do
+        expect(Excellent.read('./spec/fixtures/test_spreadsheet.xlsx', 0)).to eq(expected_output)
+      end
+    end
+  end
+
   describe "#worksheet_names" do
     it "returns worksheet names" do
-      expect(Excellent.worksheet_names('./spec/fixtures/simple_example.xlsx')).to eq({"Bibelbund_Abfrage"})
+      expect(Excellent.worksheet_names('./spec/fixtures/test_spreadsheet.xlsx')).to eq({"Text Only", "Mixed Types"})
     end
   end
 
@@ -11,37 +29,111 @@ defmodule ExcellentSpec do
     let :shared_strings do
       """
       <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-      <sst count="10" uniqueCount="8" xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+      <sst count="28" uniqueCount="28" xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
         <si>
-          <t>TITEL</t>
+          <t>1.A1</t>
         </si>
         <si>
-          <t>VERFASSER</t>
+          <t>1.B1</t>
         </si>
         <si>
-          <t>OBJEKT</t>
+          <t>1.C1</t>
         </si>
         <si>
-          <t>NUMMER</t>
+          <t>1.D1</t>
         </si>
         <si>
-          <t>SEITE</t>
+          <t>1.A2</t>
         </si>
         <si>
-          <t>INTERNET</t>
+          <t>1.B2</t>
         </si>
         <si>
-          <t>PC</t>
+          <t>1.C2</t>
         </si>
         <si>
-          <t>KENNUNG</t>
+          <t>1.D2</t>
+        </si>
+        <si>
+          <t>1.A3</t>
+        </si>
+        <si>
+          <t>1.B3</t>
+        </si>
+        <si>
+          <t>1.C3</t>
+        </si>
+        <si>
+          <t>1.D3</t>
+        </si>
+        <si>
+          <t>1.A4</t>
+        </si>
+        <si>
+          <t>1.B4</t>
+        </si>
+        <si>
+          <t>1.C4</t>
+        </si>
+        <si>
+          <t>1.D4</t>
+        </si>
+        <si>
+          <t>1.A5</t>
+        </si>
+        <si>
+          <t>1.B5</t>
+        </si>
+        <si>
+          <t>1.C5</t>
+        </si>
+        <si>
+          <t>1.D5</t>
+        </si>
+        <si>
+          <t>2.A1</t>
+        </si>
+        <si>
+          <t>2.B1</t>
+        </si>
+        <si>
+          <t>2.C1</t>
+        </si>
+        <si>
+          <t>2.D1</t>
+        </si>
+        <si>
+          <t>Text</t>
+        </si>
+        <si>
+          <t>Text with Encodingßäôé</t>
+        </si>
+        <si>
+          <t>Text with Number 123</t>
+        </si>
+        <si>
+          <t>Text with Newline
+
+
+      And so on</t>
         </si>
       </sst>
       """
     end
 
     let :expected_tuple do
-      {"TITEL", "VERFASSER", "OBJEKT", "NUMMER", "SEITE", "INTERNET", "PC", "KENNUNG"}
+      {
+        "1.A1", "1.B1", "1.C1", "1.D1",
+        "1.A2", "1.B2", "1.C2", "1.D2",
+        "1.A3", "1.B3", "1.C3", "1.D3",
+        "1.A4", "1.B4", "1.C4", "1.D4",
+        "1.A5", "1.B5", "1.C5", "1.D5",
+        "2.A1", "2.B1", "2.C1", "2.D1",
+        "Text",
+        "Text with Encodingßäôé",
+        "Text with Number 123",
+        "Text with Newline\n\n\nAnd so on"
+      }
     end
 
     it "returns expected tuple" do
